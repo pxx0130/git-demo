8c58abdd2c7e8d4bf1b91c0d26b10a945a6a3f65
pxx <2314036936@qq.com>
2022-05-12 09:55:12 +0800

    my hot-test first commit

---
+++ Demo.java
@@ -0,0 +1,7 @@
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("good,morning,lly");
+        System.out.println("good,morning,lly");
+        System.out.println("good,morning,lly,l love you");
+    }
+}
